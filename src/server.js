c882f79b53ea86c21569ba05f1db803812cdaf86
Venassa Naidoo <reecenaidoo30@gmail.com>
2022-05-06 22:36:04 +0200

    first commit

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const kebabCase = require('lodash/kebabCase');
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
